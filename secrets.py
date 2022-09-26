ae0ffb84553ff91810b20d8fb198510231567a6c
Leon Stier <Lms@gnadenweiler.de>
2022-09-26 18:51:12 +0200

    Update queues
    Removed Flex TT
    Added TFT

---
+++ secrets.py
@@ -0,0 +1,2 @@
+#riot_key = "RGAPI-ee7e7e05-f7d5-4bc7-9e16-b2b4f2317bbc"
+riot_key = "RGAPI-0713975a-130a-4746-99c3-aec861659ddb"
--- spreadsheetEloApi.py
+++ spreadsheetEloApi.py
@@ -1,21 +1,26 @@
 #coding: utf-8
 
 from __future__ import print_function
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
+from googleapiclient.errors import HttpError
 import os
 import requests
+import secrets
 
 
-scopes = "https://www.googleapis.com/auth/spreadsheets"
+SCOPES = "https://www.googleapis.com/auth/spreadsheets"
+
 
 def main():
 
 	row = 2
 
 	
-	accounts = []
+	accounts = ["Toxillo", "Sir Meldor", "Sir Synthoras", "xZylote", "Aphadon", "Sir Veredir"]
 
 	accountDict = {}
 
@@ -23,80 +28,91 @@ def main():
 
 		names = "Unranked"
 		soloQ = "Unranked"
-		flexTT = "Unranked"
 		flexSR = "Unranked"
+		tft = "Unranked"
 		
 
-		getId = requests.get("https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/"+i+"?api_key=")
+		getId = requests.get("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + i + "?api_key="  +  secrets.riot_key)
 		accountId = str(getId.json()["id"])
-		r = requests.get("https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/"+accountId+"?api_key=")
-		leagues = r.json()
-
+		lolRequest = requests.get("https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + accountId + "?api_key=" + secrets.riot_key)
+		lolLeagues = lolRequest.json()
+		tftLeagues = requests.get("https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/" + accountId + "?api_key=" + secrets.riot_key).json()
+		print(tftLeagues)
 			
 		names = getId.json()["name"]
-
+		level = getId.json()["summonerLevel"]
 		
 	
-		for l in leagues:
+		for l in lolLeagues:
 			if l["queueType"] == "RANKED_SOLO_5x5":
-				soloQ = str(l["tier"]+" "+l["rank"])
-			elif l["queueType"] == "RANKED_FLEX_TT":
-				flexTT = str(l["tier"]+" "+l["rank"])
+				soloQ = str(l["tier"] + " " + l["rank"])
 			elif l["queueType"] == "RANKED_FLEX_SR":
-				flexSR = str(l["tier"]+" "+l["rank"])
-				#file.write(getId.json()["name"]+"\n")
-				#file.write(l["queueType"]+": "+l["tier"]+"\n")
+				flexSR = str(l["tier"] + " " + l["rank"])
+				#file.write(getId.json()["name"] + "\n")
+				#file.write(l["queueType"] + ": " + l["tier"] + "\n")
+
+		for l in tftLeagues:
+			if l["queueType"] == "RANKED_TFT":
+				tft = str(l["tier"] + " " + l["rank"])
+
 		
 		accountDict = {
 			"name"	:	getId.json()["name"],
 			"soloQ"	:	soloQ,
-			"flexTT":	flexTT,
-			"flexSR":	flexSR
+			"flexSR":	flexSR,
+			"tft"	:	tft,
+			"level" :	level
 		}
 
 
 
 		printer(accountDict, row)
 
-		row += 1
+		row  += 1
 
 def printer(accDict, row):
+	creds = None
 
-	print(accDict)
+	if os.path.exists('token.json'):
+		creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-	store = file.Storage("token.json")
-	creds = store.get()
-
-    
-	if not creds or creds.invalid:
-		flow = client.flow_from_clientsecrets("credentials.json", scopes)
-		creds = tools.run_flow(flow, store)
-	service = build("sheets", "v4", http=creds.authorize(Http()))
-
-
-	data = [
-   		{
-   			"range": "Sheet1!A%s:D%s" % (row, row),
-   			"majorDimension": "ROWS",
-   			"values": [
-   						[accDict["name"], accDict["soloQ"], accDict["flexTT"], accDict["flexSR"]
-   					]
-   			]
-   					
-   		},
-
-   	]
 	
-	body = {
-		"valueInputOption": "USER_ENTERED",
-    	"data": data
-	}
-	
-	result = service.spreadsheets().values().batchUpdate(
-    spreadsheetId="1U428igWOgCWm_AfNLimqpul_VFa2z167Nm6FgFh3pmU", body=body).execute()
-	print("{0} cells updated.".format(result.get("updatedCells")));
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+		# Save the credentials for the next run
+		with open('token.json', 'w') as token:
+			token.write(creds.to_json())
+
+	try:
+		print(accDict)
+		service = build("sheets", "v4", credentials=creds)
 
 
+		data = [
+			{
+				"range": "Sheet1!A%s:E%s" % (row, row),
+				"majorDimension": "ROWS",
+				"values": [
+					[ accDict["name"], accDict["soloQ"], accDict["flexSR"], accDict["tft"], accDict["level"] ]
+				]
+						
+			},
+		]
+
+		body = {
+			"valueInputOption": "USER_ENTERED",
+			"data": data
+		}
+		
+		result = service.spreadsheets().values().batchUpdate(spreadsheetId="1U428igWOgCWm_AfNLimqpul_VFa2z167Nm6FgFh3pmU", body=body).execute()
+
+	except HttpError as err:
+		print(err)
 
 if __name__ == "__main__":
 	main()
